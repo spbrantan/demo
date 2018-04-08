66a148fcf6bd26d6ad7e2bcfd48d1cd198946158
brantansp <brantansp@brantansp-PC>
2018-04-08 15:56:18 +0530

    Initial commit

--- Demo/src/main/java/com/fss/BaseClass.java
+++ Demo/src/main/java/com/fss/BaseClass.java
@@ -1,19 +1,31 @@
 package com.fss;
 
 import org.testng.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BaseClass {
+	
+	static WebDriver driver;
 	
 	@BeforeSuite
 	public static void setup()
 	{
-		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\lib\\chromedriver2.33.exe");
+	    driver = (WebDriver) new ChromeDriver();
 	}
 	
 	@BeforeTest
 	public static void beforeTest()
 	{
 		
+	}
+	
+	@Test
+	public static void Test ()
+	{
+		driver.get("http://google.com");
 	}
 	
 	@BeforeMethod
