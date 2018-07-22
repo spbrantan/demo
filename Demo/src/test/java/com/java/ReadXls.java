1f1b1595143cdbbe5176a5c51b16fa603898eb87
brantansp <brantansp@brantansp-PC>
2018-07-22 21:05:11 +0530

    Reference #5

---
+++ Demo/src/test/java/com/java/ReadXls.java
@@ -0,0 +1,60 @@
+package com.java;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.*;
+
+public class ReadXls {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(new File(System.getProperty("User.dir"))));
+		HSSFSheet sheet = workbook.getSheetAt(0);
+		Row row = sheet.getRow(0);
+		String cell = row.getCell(0).getStringCellValue();
+		System.out.println(cell);
+		workbook.close();
+
+		Iterator<Row> iterator = sheet.rowIterator();
+		while (iterator.hasNext()) {
+             Row nxtRow = iterator.next();
+             Iterator <Cell> cellIterator = nxtRow.iterator();
+             
+             while (cellIterator.hasNext())
+             {
+            	 Cell cell1 = cellIterator.next();
+            	
+            	 switch(cell1.getCellType())
+            	 {
+            	 case Cell.CELL_TYPE_STRING:
+                   System.out.println();
+                   break;
+            	 case Cell.CELL_TYPE_BOOLEAN:
+            		 System.out.println();
+            		 break;
+            	 case Cell.CELL_TYPE_NUMERIC:
+            	     System.out.println();
+            	     break;
+            	 }
+            	 
+             }
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Demo/src/test/java/com/java/StringManipulation.java
+++ Demo/src/test/java/com/java/StringManipulation.java
@@ -3,7 +3,8 @@ package com.java;
 public class StringManipulation {
 
 	public static void main(String[] args) {
-		
+		//stringComparator();
+		stringReversal2 ();
 	}
 	
 	/**
@@ -18,5 +19,33 @@ public class StringManipulation {
 			rev = rev + str.charAt(str.length()- i-1);
 		}
 		System.out.println(rev);
+	}
+	
+	public static void stringReversal2 ()
+	{
+		String str= "bidw shi hso shh";
+		String rev= "";
+		int i ;
+		for ( i =str.length() ; 0<i; i--){
+			rev = rev + str.charAt(i-1);
+		}
+		System.out.println("Rest : "+rev);
+	}
+	/**
+	 * STRING MEMORY ALLOCATION
+	 */
+	public static void stringComparator()
+	{
+		//Memory created in string pool where variable pointed to string content
+		String s1 = "hello";
+		String s2 = "hello";
+		
+		//Memory created in heap where each string content created separately
+		String str1 = new String ("hello");
+		String str2= new String ("hello");
+		if(str1.equals(str2))
+		{
+			System.out.println("matching");
+		}
 	}
 }
