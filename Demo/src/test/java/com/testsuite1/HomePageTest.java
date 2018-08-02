fb1a63d3f8d5d4bbfb63d1a6f2bcf5aa3e698ce1
Brantan sp <Brantan sp@brantansp>
2018-08-02 23:20:10 +0530

    Reference #9 Basic Form

--- Demo/src/test/java/com/testsuite1/HomePageTest.java
+++ Demo/src/test/java/com/testsuite1/HomePageTest.java
@@ -1,10 +1,15 @@
 package com.testsuite1;
 
 import java.lang.invoke.MethodHandles;
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import org.openqa.selenium.support.ui.Select;
 import com.fss.BaseClass;
@@ -16,7 +21,7 @@ public class HomePageTest extends BaseClass{
 	
 	public static Log log = LogFactory.getLog(MethodHandles.lookup().lookupClass().getSimpleName());
 	
-	@Test
+	//@Test
 	public void alertHandling()
 	{
 		driver.findElement(By.linkText("Alert")).click();
@@ -26,7 +31,7 @@ public class HomePageTest extends BaseClass{
 		driver.navigate().back();
 	}
 	
-	@Test
+	//@Test
 	public void alertsHandling()
 	{
 		driver.findElement(By.linkText("Alerts")).click();
@@ -50,19 +55,76 @@ public class HomePageTest extends BaseClass{
 		driver.navigate().back();
 	}
 	
-	@Test
+	//@Test
 	public void basicAjax() throws InterruptedException
 	{
 		driver.findElement(By.linkText("Basic Ajax")).click();
 		Select category = new Select(driver.findElement(By.name("id")));
 		category.selectByVisibleText("Server");
 		category = new Select(driver.findElement(By.name("language_id")));
-		category.selectByVisibleText("Java");
+		category.selectByVisibleText("Flash");
 		driver.findElement(By.id("lteq30")).sendKeys("15");
 		driver.findElement(By.name("submitbutton")).click();
 		driver.findElement(By.linkText("Go back to the Ajax form")).click();
 	    driver.quit();
 	}
+	
+	//@Test(invocationCount= 1, threadPoolSize =1)
+	public void basicHtmlForm() throws InterruptedException
+	{
+		driver.findElement(By.id ("htmlform")).click();
+		driver.findElement(By.name("username")).sendKeys("BRANTANSP");
+		driver.findElement(By.name("password")).sendKeys("Test@123");
+		driver.findElement(By.name("comments")).clear();
+		driver.findElement(By.name("comments")).sendKeys("Testing the server");
+		List <WebElement> elm= driver.findElements(By.xpath("//input[@type='checkbox']"));
+		for(int i=0; i<elm.size(); i++)
+		{
+			if(elm.get(i).isSelected())
+			{
+				System.out.println(elm.get(i).getAttribute("value")+ " is selected");	
+				elm.get(i).click();
+			} else{
+				elm.get(i).click();
+			}
+		}
+		
+		List <WebElement> radioElm = driver.findElements(By.xpath("//input[@type='radio']"));
+		for(int i=0;i<radioElm.size();i++)
+		{
+			if(radioElm.get(i).isSelected())
+			{
+				System.out.println(radioElm.get(i).getAttribute("value") +" is selected");
+			}
+		}
+		radioElm.get(2).click();
+		
+		WebElement select = driver.findElement(By.name("multipleselect[]"));
+		Select option= new Select(select);
+		option.selectByVisibleText("Selection Item 1");
+		option.selectByVisibleText("Selection Item 2");
+		option.selectByVisibleText("Selection Item 4");
+		
+		WebElement dropdown = driver.findElement(By.name("dropdown"));
+		Select option2= new Select(dropdown);
+		option2.selectByVisibleText("Drop Down Item 6");
+		
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		driver.close();
+	}
+	
+	@Test
+	public void dragDrop()
+	{
+		driver.findElement(By.partialLinkText("GUI")).click();
+		
+		WebElement elm1=driver.findElement(By.id("draggable1"));
+		WebElement drp1=driver.findElement(By.id("droppable1"));
+		
+		Actions action= new Actions(driver);
+		action.clickAndHold(elm1).release(drp1).build().perform();
+	}
+	
 }
 
 
